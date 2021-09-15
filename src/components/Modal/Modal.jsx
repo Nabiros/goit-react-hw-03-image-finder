541e357b6c16baa6a313c7a3f0ce76c6ab0caec5
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-09-15 13:50:44 +0300

    added components

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import './_modal.scss';
+import PropTypes from 'prop-types';
+import { createPortal } from "react-dom";
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+    static propTypes = {
+        image: PropTypes.string,
+        onclick: PropTypes.func
+    }
+
+    componentDidMount() {
+        window.addEventListener('keydown', this.onEscClick)
+    };
+
+    componentDidUpdate() {
+        console.log('modal is did update')
+    };
+
+    onEscClick = (e) => {
+        const { onclick } = this.props;
+        if (e.code === 'Escape') {
+            onclick();
+        }
+    };
+
+    render() {
+        const { onclick, image } = this.props;
+
+        return createPortal(
+            <div className="ModalOverlay" onclick={onclick}>
+                <div className="Modal">{this.props.children}
+                    <img src={image} alt="" />
+                </div>
+            </div>,
+            modalRoot,
+        );
+    }
+};
