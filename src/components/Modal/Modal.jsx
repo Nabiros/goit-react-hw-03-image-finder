01bc8e5e9769c3d8e12918c71230459436dce1ba
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-09-21 14:54:53 +0300

    finish hw

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,38 @@
 import { Component } from "react";
-import './_modal.scss';
-
-import { createPortal } from "react-dom";
-
-const modalRoot = document.querySelector('#modal-root');
+import { Overlay, ModalStyle } from "./Modal.styled";
 
 export class Modal extends Component {
-
-    
     componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown)
-    };
-    
-    componentDidUpdate() {
-        console.log('modal is did update')
-    };
-    
+      window.addEventListener('keydown', this.handleKeyDown);
+    }
+  
     componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown)
+      window.removeEventListener('keydown', this.handleKeyDown);
     }
-    
+  
     handleKeyDown = e => {
-        if (e.code === 'Escape') {
-            this.props.onClose();
-        }
+      if (e.code === 'Escape') {
+        this.props.onSelect();
+      }
     };
-    
+  
     handleBackdropClick = e => {
-        if (e.target === e.currentTarget) {
-            this.props.onClose();
-        }
+      if (e.currentTarget === e.target) {
+        this.props.onSelect();
+      }
     };
-
+  
     render() {
-        const { onclick, image, children } = this.props;
-
-        return createPortal(
-            <div className="ModalOverlay" onclick={onclick}>
-                <div className="Modal">{children}
-                    <img src={image} alt="" />
-                </div>
-            </div>,
-            modalRoot,
-        );
+      const { src, alt } = this.props;
+     return (
+        <Overlay onClick={this.handleBackdropClick}>
+          <ModalStyle>
+            <img src={src} alt={alt} />
+          </ModalStyle>
+        </Overlay>
+    
+      );
     }
-};
+}
+
+
